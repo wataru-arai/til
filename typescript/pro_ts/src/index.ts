4401f13e3c2b5e0aa689b502027d1df542b951eb
WataruArai <amazing.phrasing.pg@gmail.com>
2024-09-30 19:22:14 +0900

    feat: ~4.6.3 number[]に決めうち

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,40 +1,12 @@
-// 自分の回答
-// アロー関数式による関数の作成
-// const getFizzBuzzString = (i: number): string => {
-//   if (i % 3 === 0 && i % 5 === 0) {
-//     return "FizzBuzz"
-//   } else if (i % 3 === 0) {
-//     return "Fizz"
-//   } else if (i % 5 === 0) {
-//     return "Buzz"
-//   } else {
-//     return String(i)
-//   }
-// }
-
-// 模範回答例
-// 関数宣言による関数の作成
-function getFizzBuzzString(i: number):string {
-  if (i % 3 === 0 && i % 5 === 0) {
-    return "FizzBuzz"
-  } else if (i % 3 === 0) {
-    return "Fizz"
-  } else if (i % 5 === 0) {
-    return "Buzz"
-  } else {
-    return String(i)
+function map(array: number[], callback: (i: number) => number): number[] {
+  const result: number [] = []
+  for (const element of array) {
+    result.push(callback(element))
   }
+  return result;
 }
 
-const sequence = (start: number, end: number):number[] => {
-  const result: number[] = []; // ここに型注釈をつけてあげる
-  for (let i = start; i < end; i++) {
-    result.push(i)
-  }
-  return result
-}
+const data = [1, 1, 2, 3, 5, 8, 13]
 
-for (const i of sequence(1, 100)) {
-  const message = getFizzBuzzString(i);
-  console.log(message)
-}
+const result = map(data, (x) => x * 10);
+console.log(result);
