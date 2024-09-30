3baf51c881253ca16bee28bb6cc31334935be93f
WataruArai <amazing.phrasing.pg@gmail.com>
2024-09-30 19:28:05 +0900

    feat: ~4.6.3 引数と返り値をジェネリクスにする 自分の回答

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,12 +1,12 @@
-function map(array: number[], callback: (i: number) => number): number[] {
-  const result: number [] = []
+function map<T, V>(array: T[], callback: (i: T) => V): V[] {
+  const result: V[] = []
   for (const element of array) {
     result.push(callback(element))
   }
   return result;
 }
 
-const data = [1, 1, 2, 3, 5, 8, 13]
+const data = [1, -3, -2, 8, 0, -1]
 
-const result = map(data, (x) => x * 10);
+const result:boolean[] = map(data, (x) => x >= 0);
 console.log(result);
