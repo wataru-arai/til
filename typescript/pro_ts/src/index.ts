36b23fde6930c5da3ef67e234bc8d9b9df1bcddb
WataruArai <amazing.phrasing.pg@gmail.com>
2024-09-30 19:03:01 +0900

    feat: ~4.6.2 模範回答

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,4 +1,20 @@
-const getFizzBuzzString = (i: number): string => {
+// 自分の回答
+// アロー関数式による関数の作成
+// const getFizzBuzzString = (i: number): string => {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     return "FizzBuzz"
+//   } else if (i % 3 === 0) {
+//     return "Fizz"
+//   } else if (i % 5 === 0) {
+//     return "Buzz"
+//   } else {
+//     return String(i)
+//   }
+// }
+
+// 模範回答例
+// 関数宣言による関数の作成
+function getFizzBuzzString(i: number):string {
   if (i % 3 === 0 && i % 5 === 0) {
     return "FizzBuzz"
   } else if (i % 3 === 0) {
@@ -11,7 +27,7 @@ const getFizzBuzzString = (i: number): string => {
 }
 
 const sequence = (start: number, end: number):number[] => {
-  const result = [];
+  const result: number[] = []; // ここに型注釈をつけてあげる
   for (let i = start; i < end; i++) {
     result.push(i)
   }
