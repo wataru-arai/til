e1583926332c168506194f5c0bf29ed7cb1e4fcb
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-01 20:03:17 +0900

    feat: pro_ts ~5.1.10

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,18 +1,20 @@
-const User = class {
-  name: string;
-  #age: number;
-
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.#age = age;
+class User {
+  static adminUser:User;
+  static {
+    this.adminUser = new User();
+    this.adminUser.#age = 9999;
   }
 
-  public isAdult():boolean {
-    return this.#age >= 20;
+  #age: number = 0;
+  getAge() {
+    return this.#age;
+  }
+  setAge(age: number) {
+    if (age < 0 || age > 150) {
+      return;
+    }
+    this.#age = age
   }
 }
 
-const uhyo = new User("uhyo", 26)
-console.log(uhyo.name);
-console.log(uhyo.isAdult());
-
+console.log(User.adminUser.getAge())
