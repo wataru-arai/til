7171f750dcab02286e37436cf711f28efac31407
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-01 19:53:36 +0900

    feat: pro_ts ~5.1.9
    - プライベート識別子（#foo）と`private foo`でコンパイルの結果が全然違う

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,11 +1,18 @@
-class User {
-  name: string = "";
-  age: number = 0;
+const User = class {
+  name: string;
+  #age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.#age = age;
+  }
+
+  public isAdult():boolean {
+    return this.#age >= 20;
+  }
 }
 
-const uhyo = new User();
+const uhyo = new User("uhyo", 26)
 console.log(uhyo.name);
-console.log(uhyo.age);
+console.log(uhyo.isAdult());
 
-uhyo.age = 26;
-console.log(uhyo.age);
