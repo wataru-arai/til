9460e8afc4f08a1e6564294a5b9c1da8f4b6a6f6
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-02 20:54:27 +0900

    feat:pro_ts ~5.2.1

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,21 +1,16 @@
-class User<T> {
-  name: string;
-  #age: number;
-  readonly data: T;
-
-  constructor(name: string, age: number, data: T) {
-    this.name = name;
-    this.#age = age;
-    this.data = data;
-  }
+// クラス式なのでUserというクラスの型は生成されない
+const User = class {
+  name: string = "";
+  age: number = 0;
 
   public isAdult(): boolean {
-    return this.#age >= 20;
+    return this.age >= 20;
   }
 }
 
-const uhyo = new User<string>("uhyo", 26, "追加データ");
-const data = uhyo.data;
-
-const john = new User("John Smith", 15, {num: 123})
-const data2 = john.data;
+const uhyo = new User();
+const john: User = {
+  name: "John Smith",
+  age: 15,
+  isAdult: () => true
+}
