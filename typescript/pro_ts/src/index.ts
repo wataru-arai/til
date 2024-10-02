116dcea37656e4636b83656ae924f89249dad71a
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-02 21:09:45 +0900

    feat:pro_ts ~5.2.3

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,17 +1,29 @@
-// クラス式なのでUserというクラスの型は生成されない
+type HasAge = {
+  age: number;
+}
+
+// UserはHasAgeの部分型
 class User {
-  name: string = "";
-  age: number = 0;
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number){
+    this.name = name;
+    this.age = age;
+  }
 }
 
-const uhyo = new User();
-console.log(uhyo instanceof User);
-console.log({} instanceof User);
-const john: User = {
-  name: "John Smith",
-  age: 15
+function getPrice(customer:HasAge) {
+  if (customer instanceof User && customer.name === "uhyo") {
+    return 0;
+  }
+  return customer.age < 18 ? 1000 : 1800;
 }
 
-// User型ではあるがUserのインスタンスではないのでfalseになる
-// ランタイムでオブジェクトがどう生成されたかを見ている(= 型は見ていない)
-console.log(john instanceof User)
+const customer1: HasAge = { age: 15 }
+const customer2: HasAge = { age: 40 }
+const uhyo = new User("uhyo", 26);
+
+console.log(getPrice(customer1))
+console.log(getPrice(customer2))
+console.log(getPrice(uhyo))
