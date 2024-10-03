94a0e37dad18f5e4342e92279281e64b2e1fe51f
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-03 21:04:00 +0900

    feat:pro_ts ~5.4.1

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,29 +1,21 @@
-type HasAge = {
-  age: number;
-}
-
-// UserはHasAgeの部分型
 class User {
   name: string;
-  age: number;
+  #age: number;
 
-  constructor(name: string, age: number){
+  constructor(name: string, age: number) {
     this.name = name;
-    this.age = age;
+    this.#age = age;
+  }
+
+  public isChild():boolean {
+    return this.#age <  20;
   }
 }
 
-function getPrice(customer:HasAge) {
-  if (customer instanceof User && customer.name === "uhyo") {
-    return 0;
+class PremiumUser extends User {
+  rank: number = 1;
+
+  public override isAdult(): boolean {
+    return true;
   }
-  return customer.age < 18 ? 1000 : 1800;
 }
-
-const customer1: HasAge = { age: 15 }
-const customer2: HasAge = { age: 40 }
-const uhyo = new User("uhyo", 26);
-
-console.log(getPrice(customer1))
-console.log(getPrice(customer2))
-console.log(getPrice(uhyo))
