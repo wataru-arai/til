66c81b6c49ffd30da38ffd2b5fb8a0404208b54e
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-05 21:06:21 +0900

    feat:pro_ts ~5.5.3

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,6 +1,13 @@
-console.log("エラーを発生させます");
-throwError();
-console.log("エラーを発生させました");
+try {
+  console.log("エラーを発生させます");
+  throwError();
+  console.log("エラーを発生させました");
+} catch(err) {
+  console.log("エラーをキャッチしました")
+  console.log(err)
+}
+
+console.log("おわり")
 
 function throwError() {
   const error = new Error("エラーが発生しました!!!!")
