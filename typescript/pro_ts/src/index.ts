5aac7db409fb00940c3f630a4bbfafc2fe200719
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-06 20:57:19 +0900

    feat:pro_ts ~5.6.4 自分の回答
    - すでに関数型の宣言の方法が分かってなかった

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -22,23 +22,36 @@
 
 
 
-class User {
-  readonly name: string;
-  readonly age: number;
+// class User {
+//   readonly name: string;
+//   readonly age: number;
 
-  constructor(name: string, age: number) {
-    if (name === "") {
-      throw new Error("名前は空にできません!")
-    }
-    this.name = name;
-    this.age = age;
-  }
+//   constructor(name: string, age: number) {
+//     if (name === "") {
+//       throw new Error("名前は空にできません!")
+//     }
+//     this.name = name;
+//     this.age = age;
+//   }
 
-  getMessage(message: string): string {
-    return `${this.name} (${this.age}) 「${message}」`
-  }
-}
+//   getMessage(message: string): string {
+//     return `${this.name} (${this.age}) 「${message}」`
+//   }
+// }
 
 // 以下が動作するようにクラスを使って上記のコードを置き換える
-const uhyo = new User("uhyo", 26);
-console.log(uhyo.getMessage("こんにちは"))
+// const uhyo = new User("uhyo", 26);
+// console.log(uhyo.getMessage("こんにちは"))
+
+// 以下が動作するようにクラスを関数に置き換える
+const getMessage = createUser("uhyo", 26)
+console.log(getMessage("こんにちは！"))
+
+type func = (val: string) => string
+
+function createUser(name: string, age: number):func {
+  const getMessage = function(val: string): string {
+    return `${name} (${age}) 「${val}」`
+  }
+  return getMessage
+}
