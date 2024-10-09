54b6f21b56662bfa184169679b32332c017141c6
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-09 20:38:03 +0900

    feat:pro_ts ~6.3.2

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,21 +1,10 @@
-type SignType = "plus" | "minus";
-
-function signNumber(type: SignType) {
-  return type === "plus" ? 1 : -1
-}
-
-function numberWithSign(num: number, type: SignType | "none") {
-  // 型が絞り込まれてないとここで以下のようなsignNumberの呼び出しはできない
-  // return num * signNumber(type)
-
-  // 等価演算子を用いることで型を絞り込む
-  if(type === "none") {
-    return 0;
+function formatNumberOrString(value: string | number) {
+  if (typeof value === "number") {
+    return value.toFixed(3)
   } else {
-    return num * signNumber(type)
+    return value
   }
 }
 
-console.log(numberWithSign(5, "plus"))
-console.log(numberWithSign(5, "minus"))
-console.log(numberWithSign(5, "none"))
+console.log(formatNumberOrString(3.14))
+console.log(formatNumberOrString("uhyo"))
