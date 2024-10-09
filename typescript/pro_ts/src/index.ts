b65cc21d384eca038780c1f5714fde17e266a6d8
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-09 20:56:35 +0900

    feat:pro_ts ~6.3.3

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,10 +1,39 @@
-function formatNumberOrString(value: string | number) {
-  if (typeof value === "number") {
-    return value.toFixed(3)
+import { get } from "http";
+
+type Animal = {
+  tag: "animal";
+  species: string;
+}
+
+type Human = {
+  tag: "human";
+  name: string;
+}
+
+type User = Animal | Human;
+
+const tama: User = {
+  tag: "animal",
+  species: "Foo"
+}
+
+const uhyo: User = {
+  tag: "human",
+  name: "uhyo"
+}
+
+const alien: User = {
+  tag: "alien",
+  name: "grey"
+}
+
+function getUserName(user: User) {
+  if (user.tag === "human") {
+    return user.name
   } else {
-    return value
+    return "名無し"
   }
 }
 
-console.log(formatNumberOrString(3.14))
-console.log(formatNumberOrString("uhyo"))
+console.log(getUserName(tama));
+console.log(getUserName(uhyo));
