ae3dd90a97525d8788796d4b8af234e14ff956ac
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-10 22:11:34 +0900

    feat:pro_ts ~6.4.2

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,58 +1,19 @@
-import { get } from "http";
-
-type Animal = {
-  tag: "animal";
-  species: string;
+const mmConversionTable = {
+  mm: 1,
+  m: 1e3,
+  km: 1e6,
 }
 
-type Human = {
-  tag: "human";
-  name: string;
-}
 
-type Robot = {
-  tag: "robot";
-  name: string;
-}
-
-type User = Animal | Human | Robot;
-
-const tama: User = {
-  tag: "animal",
-  species: "Foo"
-}
-
-const uhyo: User = {
-  tag: "human",
-  name: "uhyo"
-}
-
-const alien: User = {
-  tag: "alien",
-  name: "grey"
-}
-
-// if文を使った場合
-function getUserName1(user: User):string {
-  if (user.tag === "human") {
-    return user.name
-  } else {
-    return "名無し"
+// typeof 変数 で変数から型情報を抽出
+// keyof で抽出した型情報のうち、プロパティ名を抜き出して新たな型にする
+function convertUnits(value: number, unit: keyof typeof mmConversionTable) {
+  const mmValue = value * mmConversionTable[unit]
+  return {
+    mm: mmValue,
+    m: mmValue / 1e3,
+    km: mmValue / 1e6
   }
 }
 
-// switch文を使った場合
-function getUserName2(user: User):string {
-  switch (user.tag) {
-    case "human":
-      return user.name
-    case "animal":
-      return "名無し"
-    case "robot":
-      return `CPU ${user.name}`
-  }
-
-}
-
-console.log(getUserName(tama));
-console.log(getUserName(uhyo));
+console.log(convertUnits(5600, "m"))
