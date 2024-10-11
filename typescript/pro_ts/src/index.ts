876a1651160f5ea810893b4d55d433bcf1de7afd
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-11 20:41:52 +0900

    feat:pro_ts ~6.5.2

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,18 +1,3 @@
-function get<T, K extends keyof T & string>(obj: T, key: K): T[K] {
-  const keyName: string = key
-  return obj[key]
-}
+const names = ["uhyo", "John", "Taro"] as const;
 
-type Human = {
-  name: string,
-  age: number
-}
-
-const uhyo:Human = {
-  name: "uhyo",
-  age: 26
-}
-
-const uhyoName = get(uhyo, "name")
-const uhyoAge = get(uhyo, "age")
-const uhyoGender = get(uhyo, "gender")
+type Name = (typeof names)[number]
