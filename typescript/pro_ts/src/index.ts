08aaba1d7a99a5f3f5328d4d7bf8cb74f5230993
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-13 23:05:41 +0900

    feat:pro_ts ~6.7.2

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -4,12 +4,30 @@ type Human = {
   age: number;
 }
 
-function isHuman(value: any): value is Human {
-  if(value == null) return false;
+// function isHuman(value: any): value is Human {
+//   if(value == null) return false;
 
-  return (
-    value.type === "Human" &&
-    typeof value.name === "string" &&
-    typeof value.age === "number"
-  )
+//   return (
+//     value.type === "Human" &&
+//     typeof value.name === "string" &&
+//     typeof value.age === "number"
+//   )
+// }
+
+function assertHuman(value: any): asserts value is Human {
+  if( value == null ) {
+    throw new Error('Given value is null or undefined')
+  }
+  if (
+    value.type !== "Human" ||
+    typeof value.name !== "string" ||
+    typeof value.age !== "number"
+  ) {
+    throw new Error('Given value is not a Human')
+  }
+}
+
+function checkAndUserHuman(value: unknown) {
+  assertHuman(value)
+  const name = value.name
 }
