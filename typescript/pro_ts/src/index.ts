e482925035c6a48764babbcfc5b3f918c039d55e
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-14 18:11:29 +0900

    feat:pro_ts ~6.7.4

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,2 +1,22 @@
-type NSN = [number, string, number]
-type SNSNS = [string, ...NSN, string]
+type Fruit = "apple" | "orange" | "strawberry"
+
+type FruitNumbers = {
+  [P in Fruit]: number
+}
+
+type FruitArray = {
+  [P in Fruit]: P[]
+}
+
+// const numbers: FruitNumbers = {
+//   apple: 3,
+//   orange: 10,
+//   strawberry: 20
+// }
+
+
+const numbers: FruitArray = {
+  apple: ["apple", "apple"],
+  orange: ["orange", "orange", "orange"],
+  strawberry: []
+}
