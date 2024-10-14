7055becc0f4260f81afad372674fc939040742ef
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-14 18:50:03 +0900

    feat:pro_ts ~6.8.2 模範回答

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,3 +1,4 @@
+// 以下の世にタグ付きユニオンを宣言するのは間違ってない
 type Optional<T> = Exist<T> | NotExist<T>
 
 type Exist<T> = {
@@ -9,6 +10,22 @@ type NotExist<T> = {
   tag: "not-exist",
 }
 
-const value:Optional<number> = { tag: "exist", num: 1 }
+// タグ付きユニオンを使う時は以下のようなガード節をまず用いる ここが漏れてた
+function showNumberIfExist(obj: Optional<number>) {
+  if(obj.tag === "exist") {
+    console.log(obj.num)
+  }
+}
 
-console.log(value.num)
+// 以下のように型を使った宣言をして使う
+const four:Optional<number> = {
+  tag: "exist",
+  num: 4
+}
+
+const nothing:Optional<number> = {
+  tag: "not-exist",
+}
+
+showNumberIfExist(four);
+showNumberIfExist(nothing)
