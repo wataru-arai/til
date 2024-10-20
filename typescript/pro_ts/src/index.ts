c7f143e27d236bd3580eef48377d85de4e13ebed
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-20 23:10:34 +0900

    feat:pro_ts ~8.2.3

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,24 +1,9 @@
-import { readFileSync } from "fs";
-import path from "path"
-import { fileURLToPath } from "url";
+import { readFile } from 'fs'
+import { performance } from 'perf_hooks'
 
-const filePath = fileURLToPath(import.meta.url)
-const fileDir = path.dirname(filePath)
-const dataFile = path.join(fileDir, "../uhyo.txt")
+const startTime = performance.now()
 
-const data = readFileSync(dataFile, { encoding: "utf-8" })
-
-let count = 0
-let currentIndex = 0
-
-while(true) {
-  const nextIndex = data.indexOf("uhyo", currentIndex)
-  if(nextIndex >= 0) {
-    count++;
-    currentIndex = nextIndex + 1;
-  } else {
-    break
-  }
-}
-
-console.log(count)
+readFile("uhyo.txt", "utf8", (err, result) => {
+  const endTime = performance.now();
+  console.log(`${endTime - startTime}msかかりました`)
+})
