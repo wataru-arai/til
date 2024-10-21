9bb70e256b972727f18854540340d8d920923d9a
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-21 22:34:59 +0900

    feat:pro_ts ~8.3.2

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -2,7 +2,8 @@ import { readFile } from "fs/promises";
 
 const p = readFile("foo.txt", "utf-8")
 
-p.then((data) => {
-  console.log(data)
+p.then((result) => {
+  console.log("成功", result)
+},(error: unknown) => {
+  console.log("失敗", error)
 })
-
