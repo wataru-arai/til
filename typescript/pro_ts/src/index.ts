7acdcdf3fc08826d4ca3734365bb436eaf1c8bfe
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-21 22:37:25 +0900

    feat:pro_ts ~8.3.3

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -4,6 +4,12 @@ const p = readFile("foo.txt", "utf-8")
 
 p.then((result) => {
   console.log("成功", result)
-},(error: unknown) => {
+})
+
+p.catch((error) => {
   console.log("失敗", error)
+})
+
+p.finally(() => {
+  console.log("終わりました")
 })
