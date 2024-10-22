70a47ce805db2e042152818fc633ba91f84e980a
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-22 23:25:41 +0900

    feat:pro_ts ~8.3.6

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,14 +1,22 @@
+import { rejects } from "assert";
+import { error } from "console";
+import result from "express";
 import { readFile } from "fs/promises";
+import { resolve } from "path";
 
-const p = Promise.all([
+const sleepReject = (duration: number) => {
+  return new Promise<never>((resolve, reject) => {
+    setTimeout(reject, duration)
+  })
+}
+
+const p = Promise.race([
   readFile('foo.txt', "utf-8"),
-  readFile('bar.txt', "utf-8"),
-  readFile('baz.txt', "utf-8"),
+  sleepReject(5000)
 ])
 
-p.then((results) => {
-  const [foo, bar, baz] = results
-  console.log("foo.txt", foo)
-  console.log("bar.txt", bar)
-  console.log("baz.txt", baz)
+p.then((result) => {
+  console.log('成功', result)
+}, (error: unknown) => {
+  console.log('失敗', error)
 })
