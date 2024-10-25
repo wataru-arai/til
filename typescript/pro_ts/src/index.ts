6983926c631f5f64205dea45fb33dc132e7da400
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-25 22:36:37 +0900

    feat:pro_ts ~8.4.1

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,10 +1,18 @@
-import("fs/promises")
-  .then(({readFile}) => {
-    return readFile("foo.txt", "utf-8")
-  })
-  .then((result) => {
-    console.log(result)
-  })
-  .catch((error) => {
-    console.log("エラーが発生しました！", error)
-  })
+async function get3(): number {
+  console.log("get3が呼び出されました");
+  return 3;
+}
+
+// console.log("get3を呼び出します")
+// const p = get3();
+// p.then((num) => { console.log(`num is ${num}`)})
+
+// console.log("get3を呼び出しました")
+
+async function fail() {
+  throw new Error("Oh my god!!!!")
+}
+
+const p = fail();
+
+p.catch((err) => { console.log(err) })
