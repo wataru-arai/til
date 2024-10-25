449f8a95f05d7fd515fdf4d67cc63d1f8531c86e
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-25 23:02:18 +0900

    feat:pro_ts ~8.4.3

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,23 +1,11 @@
-const sleep = (duration: number) => {
-  return new Promise<void>((resolve) => {
-    setTimeout(resolve, duration)
-  })
+async function main() {
+  const { readFile, writeFile } = await import("fs/promises")
+
+  const fooContent = await readFile("foo.txt", "utf-8")
+  await writeFile("bar.txt", fooContent + fooContent)
+  console.log("書き込み完了しました")
 }
 
-async function get3():Promise<number> {
-  console.log("get3が呼び出されました") // 2
-  await sleep(1000);
-  console.log("awaitの次に進みました")  // 4
-  return 3;
-}
-
-console.log("get3を呼び出します") // 1
-const p = get3();
-p.then((num) => { console.log(`num is ${num}`)})  // 5
-console.log("get3を呼び出しました") // 3
-
-// console.log("get3を呼び出します")
-// console.log("get3が呼び出されました")
-// console.log("get3を呼び出しました")
-// console.log("awaitの次に進みました")
-// console.log(`num is ${num}`)
+main().then(() => {
+  console.log("main()が完了しました")
+})
