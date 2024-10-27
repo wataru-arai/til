7a76596ab0ca9afe19feae132c99e683a0fc2a71
WataruArai <amazing.phrasing.pg@gmail.com>
2024-10-27 22:32:13 +0900

    feat:pro_ts ~8.5.3 回答1

--- typescript/pro_ts/src/index.ts
+++ typescript/pro_ts/src/index.ts
@@ -1,10 +1,20 @@
-import { readFile } from "fs/promises";
 import path from "path"
+import { readFile } from "fs/promises";
 import { fileURLToPath } from "url";
+
+const sleep = (duration: number) => {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, duration)
+  })
+}
 
 const filePath = fileURLToPath(import.meta.url);
 const fileDir = path.dirname(filePath)
 const dataFile = path.join(fileDir, "../uhyo.txt")
+
+sleep(1).then(() => {
+  process.exit();
+})
 
 // top-level awaitが可能なので以下のように書ける
 const data = await readFile(dataFile, { encoding: "utf-8" })
