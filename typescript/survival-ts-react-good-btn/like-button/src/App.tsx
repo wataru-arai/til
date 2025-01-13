0d46e9d21cb3296813d3c7cc9c30e45b2e15558e
WataruArai <amazing.phrasing.pg@gmail.com>
2025-01-13 23:38:58 +0900

    feat: react-like-button-tutorial

--- typescript/survival-ts-react-good-btn/like-button/src/App.tsx
+++ typescript/survival-ts-react-good-btn/like-button/src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>TypeScriptはいいぞ</h1>
+      <LikeButton />
     </>
   );
+}
+
+function LikeButton() {
+  const [count, setCount] = useState(999);
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  return (
+    <span className="likeButton" onClick={handleClick}>
+      ♥ {count}
+    </span>
+  )
 }
 
 export default App;
