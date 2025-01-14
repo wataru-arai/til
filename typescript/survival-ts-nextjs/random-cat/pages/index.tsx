1e1c3659ca9ce9c4d7f52881debe6d5889aafd6f
WataruArai <amazing.phrasing.pg@gmail.com>
2025-01-14 23:55:30 +0900

    feat: survival-ts-nextjs

---
+++ typescript/survival-ts-nextjs/random-cat/pages/index.tsx
@@ -0,0 +1,33 @@
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+
+const IndexPage: NextPage = () => {
+  const [imageUrl, setImageUrl] = useState("")
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url)
+      setLoading(false)
+    })
+  }, [])
+
+  return <div>{loading || <img src={imageUrl} />}</div>
+}
+
+export default IndexPage;
+
+type Image = {
+  url: string
+}
+
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images)
+  return images[0]
+}
+
+fetchImage().then((image) => {
+  console.log(image.alt)
+})
